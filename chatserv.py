691a6a71fd6b1a651ac6ba17baf74271eb0fe1d9
Darkvalnar <57048928+Darkvalnar@users.noreply.github.com>
2020-05-31 14:58:15 +0200

    Update chatserv.py
    -removed clutter
    -fixed code
    -added defaults and system args

--- chatserv.py
+++ chatserv.py
@@ -5,102 +5,91 @@ __contact__ = 'sh0t@hashbang.sh'
 
 import socket
 import threading
-import argparse
+import sys
+import time
 
 
-# setup tasks for cli args
+# declaring host and port by checking for commandline args, if none are specified we will default to localhost:55555
+host = str(sys.argv[1])
+port = int(sys.argv[2])
 
-text = "Run without args to run server on 127.0.0.1 and port 55555"
-parser = argparse.ArgumentParser(description=text)
+if len(sys.argv) != 3: 
+    print ("using 127.0.0.1 and port 55555 as default values")
+    host = "127.0.0.1"
+    port = 55555
 
-# setting up cli args here:
-parser.add_argument("-ip", "--host", help="specify host IP", type=str )
-parser.add_argument("-p", "--port", help="specify host port", type=int )
-
-args = parser.parse_args()
-
-
-#Server setup task
-def setup():
-    print ("setting server in listen mode...")
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # this is where the magic happens, we define the socket and protocol for our server
-    server.bind((host, port))
-    server.listen()
-    print ("server listening on IP: " + host + " and port " + str(port))
-
-    # setting up lists to store clients and nicknames in, this is important because we will be appending this constantly
-
-    clients= []
-    nicknames = []
+# server start up
+print ("setting server in listen mode...")
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # this is where the magic happens, we define the socket and protocol for our server
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
     
-    return clients, nicknames, server
+server.bind((host, port))
+server.listen()
+    
+print ("server listening on: " + host + ":" + str(port))
+time.sleep(1)
 
-# setting up a broadcast system to send messages to all clients
+# setting up lists to store clients and nicknames in, this is important because we will be appending this constantly
+clients= []
+nicknames = []
+messagelist = []
+
+# this is where we set the actual messaging system up, this function allows us to broadcast to all clients connected
+print("setting up messaging...")
 def broadcast(message):
-    clients = setup()
-    print("setting up broadcasting...")
     for client in clients:
         client.send(message)
 
 
+
+print ("setting up message handling...")
+time.sleep(1)
+
 # Function to handle incoming client messages
 def handle(client):
-    clients, nicknames, server = setup()
-    print ("setting up message handling...")
     while True:
         try:
             # Getting incoming message and sending it to everyone else
             message = client.recv(1024)
+            #testmes = client.recv(1024)
+            #name = nicknames[index]
+            print (message) # shows messages in server log
             broadcast(message)
+            #extra(testmes)
         except:
             #Removing clients and closing them when we encounter an exception within our while loop
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast("{} has left the server!".format(nickname).encode("ascii"))
-
+            broadcast("{} has left the server! \n".format(nickname).encode("utf8"))
 
 # Function to receive and listen
+print("setting up service to receive and listen...")
+time.sleep(1)
 def receive():
-    clients, nicknames, server = setup()
-    print("setting up service to receive and listen...")
     while True: 
         # we are running this in an endless while loop to constantly accept connections
         client, address = server.accept()
         print("Connected with {}".format(str(address)))
         
         #Requesting nickname from client, if we get one, we store it for further usage
-        client.send("NICK".encode("ascii"))
-        nickname = client.recv(1024).decode("ascii")
+        client.send("NICK".encode("utf8"))
+        nickname = client.recv(1024).decode("utf8")
+        # adding nicknames and clients to the list
         nicknames.append(nickname)
         clients.append(client)
         
-        # Printing and broadcasting the nicks
+        # Printing nicks to server log and to client side
         
         print("Nickname is {}".format(nickname))
-        broadcast("{} has joined..".format(nickname).encode("ascii"))
-        client.send("Connected to the server".encode("ascii"))
+        broadcast("{} has joined..".format(nickname).encode("utf8"))
+        client.send("Connected to the server".encode("utf8"))
         
         # starting thread for client message handling
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+        #extra_thread = threading.Thread(target=extra)
+        #extra_thread.start()
 
-
-# Conditional branches to check for CLI args
-
-if args.host:
-    if args.port:
-        host = args.host
-        port = int(args.port)
-        print("Setting server in listen mode on: " + host + " with port " + str(port))
-        receive()
-    else:
-        host = args.host
-        print("Setting server in listen mode on IP:" + host + " and port 55555")
-        port = 55555
-        receive()
-else:
-    host = "127.0.0.1"
-    port = 55555
-    receive()
+receive()
